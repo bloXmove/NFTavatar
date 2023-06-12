4ec35287e973f6a5635a31eb1c914d3118b7ab3f
marvin.10xdev <marvin.10xdev@proton.me>
2023-06-12 12:57:06 +0300

    updated QR quest contract

--- contracts/demo-avatars/QRQuest.sol
+++ contracts/demo-avatars/QRQuest.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
-import "../../interfaces/INFTicket.sol";
-import './BloXmoveTestToken.sol';
+// import './BloXmoveTestToken.sol';
+
+interface IDemoServiceContract {
+	function mintNfticket(address user, string calldata URI) external returns(uint256);
+}
 
 error MintingIsPaused();
 
-contract BloXmoveQRQuest is Ownable {
+contract QRQuest is Ownable {
 
 	using Strings for uint256;
 
@@ -24,8 +27,8 @@ contract BloXmoveQRQuest is Ownable {
 
 	mapping(address => uint256) public balance;
 
-	INFTicket public NFTicketContract;
-	address public erc20_token;
+	IDemoServiceContract public DemoServiceContract;
+	// address public erc20_token;
 
 	uint256 constant TOKEN_COUNT = 8;
 
@@ -34,10 +37,10 @@ contract BloXmoveQRQuest is Ownable {
 
 	constructor(
 		string memory _initBaseURI,
-		INFTicket NFTicketContractAddress
+		IDemoServiceContract demo_contract_addr
 	) Ownable() {
 		setBaseURI(_initBaseURI);
-		setNFTickerAddress(NFTicketContractAddress);
+		DemoServiceContract = demo_contract_addr;
 	}
 
 	// public
@@ -46,32 +49,17 @@ contract BloXmoveQRQuest is Ownable {
 
 		require(ownership[msg.sender] == 0);
 
-		uint256 rand_id = (block.prevrandao % TOKEN_COUNT) + 1; // random token ID between 1 and TOKEN_COUNT
-		string memory uri = string(abi.encodePacked(baseURI, rand_id.toString(), '.json'));
+		uint256 rand_local_id = (block.prevrandao % TOKEN_COUNT) + 1; // random token ID in range [1..TOKEN_COUNT]
+		string memory uri = string(abi.encodePacked(baseURI, rand_local_id.toString(), '.json'));
 
-        Ticket memory ticket = NFTicketContract.mintNFTicket(msg.sender, getTicketParams(msg.sender, uri));
-		ownership[msg.sender] = ticket.ticketID;
-		token2wallet[ticket.ticketID] = msg.sender;
-		tokenIdMap[ticket.ticketID] = rand_id;
+        uint256 ticketID = DemoServiceContract.mintNfticket(msg.sender, uri);
+		ownership[msg.sender] = ticketID;
+		token2wallet[ticketID] = msg.sender;
+		tokenIdMap[ticketID] = rand_local_id;
 
-		BloXmoveTestToken(erc20_token).mint(msg.sender, 100 ether); // mint some test tokens to play with
+		// BloXmoveTestToken(erc20_token).mint(msg.sender, 100 ether); // mint some test tokens to play with
 
 	}
-
-	function getTicketParams(address _recipient, string calldata _URI) internal view returns (Ticket memory ticket) {
-        ticket.tokenID = 0; // This Id will be assigned correctly in the NFTicket contract
-        ticket.serviceProvider = address(this);
-        ticket.serviceDescriptor = 0x00000000; // @todo
-        ticket.issuedTo = _recipient;
-        ticket.certValue = 0;
-        ticket.certValidFrom = 0;
-        ticket.price = 0;
-        ticket.credits = 0; // @todo?
-        ticket.pricePerCredit = 0;
-        ticket.serviceFee = 900 * 1 ether;
-        ticket.resellerFee = 100 * 1 ether;
-        ticket.transactionFee = 0 * 1 ether;
-    }
 
 	function register(uint256 id) external { // register an attendance
 		if (!registry[msg.sender][id]) {
@@ -80,8 +68,8 @@ contract BloXmoveQRQuest is Ownable {
 		}
 	}
 
-	function tokenURI(uint256 tokenID) public view virtual override returns (string memory) {
-		require(token2wallet[tokenID], 'URI query for nonexistent token');
+	function tokenURI(uint256 tokenID) public view virtual returns (string memory) {
+		require(tokenIdMap[tokenID] > 0, 'URI query for nonexistent token');
 		uint256 local_token_id = tokenIdMap[tokenID];
 		uint256 lvl = level(token2wallet[tokenID]);
 		string memory level_suffix = '';
@@ -99,7 +87,7 @@ contract BloXmoveQRQuest is Ownable {
 		else return 3;
 	}
 
-	function topup(uint256 amount) external {
+	/* function topup(uint256 amount) external {
 		BloXmoveTestToken(erc20_token).transferFrom(msg.sender, address(this), amount);
 		balance[msg.sender] += amount;
 		emit TopUp(msg.sender, amount);
@@ -114,17 +102,13 @@ contract BloXmoveQRQuest is Ownable {
 		require(amount <= balance[msg.sender]);
 		balance[msg.sender] -= amount;
 		BloXmoveTestToken(erc20_token).transfer(to, amount);
-	}
+	} */
 
 	// only owner
 
-	function setNFTickerAddress(address addr) external onlyOwner {
-		nfticket_addr = addr;
-	}
-
-	function setERC20address(address addr) external onlyOwner {
+	/* function setERC20address(address addr) external onlyOwner {
 		erc20_token = addr;
-	}
+	} */
 
 	function setBaseURI(string memory _newBaseURI) public onlyOwner {
 		baseURI = _newBaseURI;
